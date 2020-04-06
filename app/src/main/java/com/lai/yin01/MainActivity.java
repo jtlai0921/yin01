c282441b9f3f6487cceb475f3db6c495552ada67
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-04-06 15:34:23 +0800

    1

--- app/src/main/java/com/lai/yin01/MainActivity.java
+++ app/src/main/java/com/lai/yin01/MainActivity.java
@@ -2,43 +2,37 @@ package com.lai.yin01;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
-private int mQuantity=0;
+    private int mQuantity=0;
     private int mPrice=5;
     private TextView mQuantityTextView;
+    private TextView mOrderSummaryTextView;
     private TextView mTotalPriceTextView;
+    private CheckBox mtoppingscheckbox;
     private StringBuilder mTotalPriceString=new StringBuilder();
+    private boolean 加酒精;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mQuantityTextView=findViewById(R.id.quantity_text_view);
-        mTotalPriceTextView=findViewById(R.id.quantity_text_view);
+        mTotalPriceTextView=findViewById(R.id.price_text_view);
+        mOrderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        mtoppingscheckbox=findViewById(R.id.toppings_checkbox);
     }
     public void submitOrder(View view){
+
         displayTotalPrice();
     }
-
-    private void displayTotalPrice() {
-
-        int total=mPrice*mQuantity;
-       int end=mTotalPriceString.length();
-       mTotalPriceString.delete(0,end);
-       mTotalPriceString.append("NT$").append(total);
-       mTotalPriceString.append(mQuantity==0?"\nFree":"\nThank you!");
-       mQuantityTextView.setText(mTotalPriceString);
-    }
-
-    private void display(int number) {
-        mQuantityTextView.setText(String.valueOf(number));
-    }
-
     public void increment(View view){
-        display(++mQuantity);
+        if(mQuantity<=3)
+        {
+            display(++mQuantity);
+        }
+
     }
     public void decrement(View view){
         if(mQuantity<=1)
@@ -47,4 +41,52 @@ private int mQuantity=0;
         }
         display(--mQuantity);
     }
+    private void display(int number) {
+        mQuantityTextView.setText(String.valueOf(number));
+    }
+    private void resetMessage(){
+        int len=mTotalPriceString.length();
+        mOrderSummaryTextView.setText("price");
+        mTotalPriceTextView.setText("NT$5");
+    }
+    private void displayTotalPrice() {
+        int total=mPrice*mQuantity;
+        mOrderSummaryTextView.setText("order");
+        mTotalPriceString.append("123\n").append("口罩\n").append("加酒精\n").append(加酒精);
+        if(mQuantity>0){
+            mTotalPriceString.append("\nQuantity: ").append(mQuantity).append("\nTotal: ").append("NT$:").append(total);
+        }
+        mTotalPriceString.append(mQuantity==0?"\nFree":"\nThank you!");
+        mQuantityTextView.setText(mTotalPriceString);
+    }
+    public void clickTopping(View view){
+       if (mtoppingscheckbox.isChecked()){
+           加酒精=true;
+       }else {
+           加酒精=false;
+       }
+    }
+public void clickManager(View view){
+        resetMessage();
+        int id=view.getId();
+        switch (id){
+
+            case R.id.toppings_checkbox:
+            clickTopping(view);
+            break;
+            case R.id.btn_inc:
+                increment(view);
+                break;
+            case R.id.btn_dec:
+                decrement(view);
+                break;
+            case R.id.btn_order:
+                submitOrder(view);
+                break;
+        }
+}
+
+
+
+
 }
